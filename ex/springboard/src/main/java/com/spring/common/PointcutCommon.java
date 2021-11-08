2007145a20c867dbe779d54394a19536745d318a
CodingPythonMan <robolab1902@naver.com>
2021-11-08 16:06:56 +0900

    Spring MVC 모델 준비했습니다.

---
+++ ex/springboard/src/main/java/com/spring/common/PointcutCommon.java
@@ -0,0 +1,13 @@
+package com.spring.common;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class PointcutCommon {
+	@Pointcut("execution(* com.spring..*Impl.*(..))")
+	public void allPointcut() {}
+
+	@Pointcut("execution(* com.spring..*Impl.get*(..))")
+	public void getPointcut() {}
+}
