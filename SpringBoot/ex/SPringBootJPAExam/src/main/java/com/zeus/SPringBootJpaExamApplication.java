467dc239edafae5e66c358ef0c0f8cef0838127e
KImJuYoung <robolab1902@naver.com>
2022-01-05 13:34:39 +0900

    JPA 예제 추가

---
+++ SpringBoot/ex/SPringBootJPAExam/src/main/java/com/zeus/SPringBootJpaExamApplication.java
@@ -0,0 +1,13 @@
+package com.zeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SPringBootJpaExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SPringBootJpaExamApplication.class, args);
+	}
+
+}
