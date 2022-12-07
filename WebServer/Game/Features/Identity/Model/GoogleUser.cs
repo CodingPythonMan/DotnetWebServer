2bc75d588fbe415a90c032d79a1894ac5793a5fb
JuYoung Kim <robolab1902@naver.com>
2022-12-07 17:21:51 +0900

    CascadingAuth 추가, SameSite 좀더 정확히 수정

---
+++ WebServer/Game/Features/Identity/Model/GoogleUser.cs
@@ -0,0 +1,13 @@
+﻿namespace Game.Features.Identity.Model
+{
+    public class GoogleUser
+    {
+        public string ID { get; set; } = null!;
+        public string AccessToken { get; set; } = null!;
+        public string RefreshToken { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+    }
+}
