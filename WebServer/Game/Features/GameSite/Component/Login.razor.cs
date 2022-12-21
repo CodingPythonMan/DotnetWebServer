5834e66724b50a1acb7393ec69ee616283dd2590
JuYoung Kim <robolab1902@naver.com>
2022-12-21 15:18:49 +0900

    괜히 프로그램 무거워지는 Radzen 제거

--- WebServer/Game/Features/GameSite/Component/Login.razor.cs
+++ WebServer/Game/Features/GameSite/Component/Login.razor.cs
@@ -5,12 +5,8 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc.Formatters;
-using Radzen;
-using Shared.Base.Common;
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices;
 
 namespace Game.Features.GameSite.Component
 {
@@ -23,10 +19,6 @@ namespace Game.Features.GameSite.Component
         private UserManager<GameSiteUser> _UserManager { get; set; } = null!;
         [Inject]
         private IDataProtectionProvider _DataProtectionProvider { get; set; } = null!;
-        [Inject]
-        private NotificationService _NotificationService { get; set; } = null!;
-        [Inject]
-        DialogService _DialogService { get; set; } = null!;
 
         public class LoginModel
         {
@@ -41,117 +33,36 @@ namespace Game.Features.GameSite.Component
 
         private async Task _OnSubmit(EditContext context)
         {
-            if (context.Validate() is false)
-            {
-                string message = context.GetValidationMessages().FirstOrDefault()!;
+            var user = await _UserManager.FindByEmailAsync(_input.Email);
 
-                if (false == string.IsNullOrEmpty(message))
-                {
-                    ShowNotify(message, NotificationSeverity.Error);
-                    return;
-                }
+            if (user is null)
+            {
+                return;
             }
 
-            ErrorResult result = await LoginProcess();
+            bool isValid = await _UserManager.CheckPasswordAsync(user, _input.Password);
 
-            switch (result)
+            if (isValid is false)
             {
-                case ErrorResult.SUCCESS:
-                    break;
-                case ErrorResult.ERROR_USER_NOT_FOUND:
-                    ShowNotify("ERROR_USER_NOT_FOUND", NotificationSeverity.Error);
-                    break;
-                case ErrorResult.ERROR_INVAILD_PASSWORD:
-                    ShowNotify("ERROR_INVAILD_PASSWORD", NotificationSeverity.Error);
-                    break;
-                default:
-                    ShowNotify(result.ToString(), NotificationSeverity.Error);
-                    break;
+                return;
             }
-        }
 
-        private async Task _OnLogin(LoginArgs args)
-        {
-            _input.Email = args.Username;
-            _input.Password = args.Password;
+            var token = await _UserManager.GenerateUserTokenAsync(user, TokenOptions.DefaultProvider, "SignIn");
 
-            ErrorResult result = await LoginProcess();
+            var returnUrl = "/";
 
-            switch (result)
-            {
-                case ErrorResult.SUCCESS:
-                    break;
-                case ErrorResult.ERROR_USER_NOT_FOUND:
-                    ShowNotify("ERROR_USER_NOT_FOUND", NotificationSeverity.Error);
-                    break;
-                case ErrorResult.ERROR_INVAILD_PASSWORD:
-                    ShowNotify("ERROR_INVAILD_PASSWORD", NotificationSeverity.Error);
-                    break;
-                default:
-                    ShowNotify(result.ToString(), NotificationSeverity.Error);
-                    break;
-            }
-        }
+            var data = $"{user.Id}|{token}|{returnUrl}";
 
+            var protector = _DataProtectionProvider.CreateProtector("SignIn");
 
+            var passData = protector.Protect(data);
 
-        private async Task<ErrorResult> LoginProcess()
-        {
-            ErrorResult result = ErrorResult.FAIL;
-
-            do
-            {
-                var user = await _UserManager.FindByEmailAsync(_input.Email);
-
-                if (user == null)
-                {
-                    result = ErrorResult.ERROR_USER_NOT_FOUND;
-                    break;
-                }
-
-                bool isVaild = await _UserManager.CheckPasswordAsync(user, _input.Password);
-
-                if (false == isVaild)
-                {
-                    result = ErrorResult.ERROR_INVAILD_PASSWORD;
-                    break;
-                }
-
-                var token = await _UserManager.GenerateUserTokenAsync(user, TokenOptions.DefaultProvider, "SignIn");
-                var returnUrl = "/";
-                var data = $"{user.Id}|{token}|{returnUrl}";
-
-                var protector = _DataProtectionProvider.CreateProtector("SignIn");
-                var passData = protector.Protect(data);
-
-                _NavigationManager.NavigateTo($"/account/signin?data={passData}", true);
-
-                result = ErrorResult.SUCCESS;
-
-            } while (false);
-
-            return result;
-        }
-
-        void ShowNotify(string message, NotificationSeverity severity = NotificationSeverity.Success)
-        {
-            _NotificationService.Notify(new NotificationMessage()
-            {
-                Severity = severity,
-                Summary = message,
-            });
+            _NavigationManager.NavigateTo($"/account/signin?data={passData}", true);
         }
 
         public void GoogleLogin()
         {
             _NavigationManager.NavigateTo($"/account/googlesignin", true);
-        }
-
-        public async Task _Register()
-        {
-            await _DialogService.OpenAsync<RegisterDialog>("Closeable from overlay Dialog",
-                 new Dictionary<string, object>(), 
-                 new DialogOptions() { Width = "700px", Height = "512px", Resizable = true, Draggable = true });
         }
     }
 }
