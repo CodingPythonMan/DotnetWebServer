17da88dd3652d57bfaaf59fc854c902ca6791848
JuYoung Kim <robolab1902@naver.com>
2023-01-09 15:48:10 +0900

    Navbar 수정

--- WebServer/Game/wwwroot/js/Navbar.js
+++ WebServer/Game/wwwroot/js/Navbar.js
@@ -4,15 +4,19 @@
 
         let account_btn = document.querySelector(".account_btn");
         let account_show = document.querySelector("nav ul .account_show");
-        let first = document.querySelector(".first");
+        let account_span = document.querySelector(".account_span");
 
         let inventory_btn = document.querySelector(".inventory_btn");
         let inventory_show = document.querySelector("nav ul .inventory_show");
-        let second = document.querySelector(".second");
+        let inventory_span = document.querySelector(".inventory_span");
 
         let item_btn = document.querySelector(".item_btn");
         let item_show = document.querySelector("nav ul .item_show");
-        let third = document.querySelector(".third");
+        let item_span = document.querySelector(".item_span");
+
+        let chatting_btn = document.querySelector(".chatting_btn");
+        let chatting_show = document.querySelector("nav ul .chatting_show");
+        let chatting_span = document.querySelector(".chatting_span");
 
         [].forEach.call(navUlLis, function (navUlLi) {
             navUlLi.addEventListener("click", function () {
@@ -25,19 +29,28 @@
             });
         });
         
-        account_btn.addEventListener('click', function () {
-           account_show.classList.toggle('show');
-           first.classList.toggle('rotate');
+        account_btn.addEventListener('click', function (e) {
+            account_show.classList.toggle('show');
+            account_span.classList.toggle('rotate');
+            e.preventDefault();
         });
 
-        inventory_btn.addEventListener('click', function () {
+        inventory_btn.addEventListener('click', function (e) {
             inventory_show.classList.toggle('show');
-            second.classList.toggle('rotate');
+            inventory_span.classList.toggle('rotate');
+            e.preventDefault();
         });
 
-        item_btn.addEventListener('click', function () {
+        item_btn.addEventListener('click', function (e) {
             item_show.classList.toggle('show');
-            third.classList.toggle('rotate');
+            item_span.classList.toggle('rotate');
+            e.preventDefault();
+        });
+
+        chatting_btn.addEventListener('click', function (e) {
+            chatting_show.classList.toggle('show');
+            chatting.classList.toggle('rotate');
+            e.preventDefault();
         });
     }
 }
