fa5eb344bc226c3ac22d5f6e97aa82a43efb86f0
JuYoung Kim <robolab1902@naver.com>
2023-02-09 17:52:21 +0900

    Network 라이브러리 구성 중... 만약 참조 없이 만드려면 어떻게 만들 것인가? TCP 책을 보면서 하나하나 만드는게 낫나?

---
+++ WebServer/Shared/Features/Network/Session.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Features.Network
+{
+    public abstract class Session
+    {
+        Socket _socket = null!;
+        int _disconnected = 0;
+
+        RecvBuffer _recvBuffer = new RecvBuffer(65535);
+
+        object _lock = new object();
+        Queue<ArraySegment<byte>> _sendQueue = new Queue<ArraySegment<byte>>();
+        List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+        SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
+
+        public abstract void OnConnected(EndPoint endPoint);
+        public abstract int OnRecv(ArraySegment<byte> buffer);
+        public abstract void OnSend(int numOfBytes);
+        public abstract void OnDisconnected(EndPoint endPoint);
+        
+    }
+}
