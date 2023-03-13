86e8697bc1df7f1a22645314338221d246648747
JuYoung Kim <robolab1902@naver.com>
2023-03-13 18:52:15 +0900

    cpp 서버와 통신할 패킷 설계 후 만드는 중

---
+++ WebServer/Game/Features/Connect/Server/ServerPacketManager.cs
@@ -0,0 +1,17 @@
+﻿namespace Game.Features.Connect.Server
+{
+    class PacketManager
+    {
+        #region Singleton
+        static PacketManager _instance = new PacketManager();
+        public static PacketManager Instance { get { return _instance; } }
+        #endregion
+
+        PacketManager()
+        {
+            
+        }
+
+        //Dictionary<ushort, Action<PacketSession>
+    }
+}
---
+++ WebServer/Game/Services/ServerSession/GameSessionService.cs
@@ -0,0 +1,30 @@
+﻿using Shared.Features.Network;
+using System.Net;
+
+namespace Game.Services.ServerSession
+{
+    public class GameSessionService : PacketSession
+    {
+        
+
+        public override void OnConnected(EndPoint endPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
